d92bab5c27db32439148ad56f84694544f5240f6
Sonu Singh Chahar <sonuchahar511@gmail.com>
2019-11-12 01:42:06 +0530

    reusable component poc

---
+++ hello-world-pocs-reusable-components/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { FavoriteChangedEventArgs } from './favorite/favorite.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'Angular App';
+
+  post={
+    title : 'Title',
+    isFavorite : true
+  }
+
+  onFavChanged(eventArngs:FavoriteChangedEventArgs){
+    alert("Favorite onFavChanged:: "+eventArngs.newValue);
+  }
+}
---
+++ hello-world-pocs-reusable-components/src/app/readme2.txt
@@ -0,0 +1,13 @@
+alert
+console
+prompt  window.prompt("sometext","defaultText");
+window.confirm("sometext");
+
+
+define input property-
+    import input decorator and add @Input() on property.
+        or by using component decorator metadata.. mention property name as inputs.
+
+    NOTE: ALWAYS USE INPUT DECORATOR AND WITH ALIAS. update component template 
+    when changing property name in class.
+
